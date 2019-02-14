98b29f2d603449ed54f209481428b2ee78399c65
ShakeelUrRehman <shakeelzain89@gmail.com>
2019-02-14 18:53:01 +0500

    Updated Changes

--- src/Pages/Home/view.js
+++ src/Pages/Home/view.js
@@ -18,66 +18,66 @@ export default class view extends Component {
       <div id="content">
       <Header/>
       <div className="container-fluid">
-      <div class="row" style={{marginBottom:"20px"}}>
-      <div class="col"><div class="card">
-      <h4 class="card-title">Doctors</h4>
-              <div class="card-body">
-                <p class="card-text">There are currently 9 Doctors Registgerd in ou database</p>
-                <a href="/DoctorDetails" class="btn btn-primary">Doctor Details</a>
+      <div className="row" style={{marginBottom:"20px"}}>
+      <div className="col"><div className="card">
+      <h4 className="card-title">Doctors</h4>
+              <div className="card-body">
+                <p className="card-text">There are currently 9 Doctors Registgerd in ou database</p>
+                <a href="/DoctorDetails" className="btn btn-primary">Doctor Details</a>
               </div>
             </div>
             </div>
-            <div class="col"><div class="card bg-warning text-white">
-            <h4 class="card-title">Medicines</h4>
-              <div class="card-body">
-                <p class="card-text">There are Toal 9 Meicines Regisgterd in Our Database.</p>
-                <a href="/Medicnes" class="btn btn-primary">Mediciness</a>
+            <div className="col"><div className="card bg-warning text-white">
+            <h4 className="card-title">Medicines</h4>
+              <div className="card-body">
+                <p className="card-text">There are Toal 9 Meicines Regisgterd in Our Database.</p>
+                <a href="/Medicnes" className="btn btn-primary">Mediciness</a>
               </div>
             </div>
             </div>
-            <div class="col"><div class="card bg-success text-white" >
-            <h4 class="card-title">Appointments</h4>
-              <div class="card-body">
-                <p class="card-text">There are Total 8 Appointments in  the database  <br></br>
+            <div className="col"><div className="card bg-success text-white" >
+            <h4 className="card-title">Appointments</h4>
+              <div className="card-body">
+                <p className="card-text">There are Total 8 Appointments in  the database  <br></br>
                 </p>
-                <a href="/Appointments" class="btn btn-primary">Appointments</a>
+                <a href="/Appointments" className="btn btn-primary">Appointments</a>
               </div>
             </div>
             </div>
 </div>
-<div class="row" style={{marginBottom:"20px"}}>
-      <div class="col"><div class="card bg-secondary text-white">
-      <h4 class="card-title">Categories</h4>
-              <div class="card-body">
-                <p class="card-text">There are currently 9 Doctors Categories Registgerd in ou database</p>
-                <a href="/DoctorCategories" class="btn btn-primary">Doctor Categories</a>
+<div className="row" style={{marginBottom:"20px"}}>
+      <div className="col"><div className="card bg-secondary text-white">
+      <h4 className="card-title">Categories</h4>
+              <div className="card-body">
+                <p className="card-text">There are currently 9 Doctors Categories Registgerd in ou database</p>
+                <a href="/DoctorCategories" className="btn btn-primary">Doctor Categories</a>
               </div>
             </div>
             </div>
-            <div class="col"><div class="card bg-primary text-white">
-            <h4 class="card-title">Orders</h4>
-              <div class="card-body">
-                <p class="card-text">There are Toal 9 Orders Regisgterd in Our Database.</p>
-                <a href="/OrdeMedicines" class="btn btn-primary">Orde Medicines</a>
+            <div className="col"><div className="card bg-primary text-white">
+            <h4 className="card-title">Orders</h4>
+              <div className="card-body">
+                <p className="card-text">There are Toal 9 Orders Regisgterd in Our Database.</p>
+                <a href="/OrdeMedicines" className="btn btn-primary">Orde Medicines</a>
               </div>
             </div>
             </div>
-            <div class="col"><div class="card bg-danger text-white" >
-            <h4 class="card-title">Patients</h4>
-              <div class="card-body">
-                <p class="card-text">There are Total 8 Patients in  the database  <br></br>
+            <div className="col"><div className="card bg-danger text-white" >
+            <h4 className="card-title">Patients</h4>
+              <div className="card-body">
+                <p className="card-text">There are Total 8 Patients in  the database  <br></br>
                 </p>
-                <a href="/patients" class="btn btn-primary">Appointments</a>
+                <a href="/patients" className="btn btn-primary">Appointments</a>
               </div>
             </div>
             </div>
 </div>
-<div class="row">
-      <div class="col"><div class="card bg-info text-white">
-      <h4 class="card-title">Doctor At Home</h4>
-              <div class="card-body">
-                <p class="card-text">There are currently 9 Doctors Registgerd in ou database</p>
-                <a href="/DoctorDetails" class="btn btn-primary">Doctor Details</a>
+<div className="row">
+      <div className="col"><div className="card bg-info text-white">
+      <h4 className="card-title">Doctor At Home</h4>
+              <div className="card-body">
+                <p className="card-text">There are currently 9 Doctors Registgerd in ou database</p>
+                <a href="/DoctorDetails" className="btn btn-primary">Doctor Details</a>
               </div>
             </div>
             </div>
